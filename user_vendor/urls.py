aa53354bc8c54f280514cfc02cb5cf88ab387251
amithuu <amithkulkarni98@gmail.com>
2024-05-30 11:00:03 +0530

    feat:'done iwith performance creation,'
    Performance creation is done, try with optimization.

--- user_vendor/urls.py
+++ user_vendor/urls.py
@@ -4,5 +4,13 @@ from user_vendor.views import *
 urlpatterns = [
     path('api/vendors/', VendorListCreateView.as_view(),name='vendors_list_create'),
     path('api/vendors/<vendor_id>/', VendorRetrieveUpdateDestroyView.as_view(), name='vendor-retrieve-update-destroy'),
+    
+    path('api/purchase_orders/', PurchaseOrderListCreateView.as_view(), name='purchase-orders-list-create'),
+    path('api/purchase_orders/<po_id>', PurchaseOrderRetrieveUpdateDestroyView.as_view(), name='purchase-orders-retrieve-update-destroy'),
 
+    path('api/vendors/<vendor_id>/performance/', VendorPerformanceRetrieveView.as_view(), name='vendor-performance-retrieve'),
+    path('api/purchase_orders/<pk>/acknowledge/', AcknowledgeUpdate.as_view()),
+    
+    path('login/', CustomAuthToken.as_view())
+    
 ]
